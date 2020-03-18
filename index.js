2954d06989678c68065688b649fb8ef187e4abe6
David Bernal <dbl1987@gmail.com>
2020-03-18 12:40:41 +0100

    Pure standar csv

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 function CSVtoArray(text) {
-  const reValid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
-  const reValue = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
-  // Return NULL if input string is not well formed CSV string.
-  if (!reValid.test(text)) return null;
-  const a = []; // Initialize array to receive values.
-  text.replace(
-    reValue, // "Walk" the string using replace with callback.
-    (m0, m1, m2, m3) => {
-      // Remove backslash from \' in single quoted values.
-      if (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
-      // Remove backslash from \" in double quoted values.
-      else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
-      else if (m3 !== undefined) a.push(m3);
-      return ""; // Return empty string.
-    }
-  );
-  // Handle special case of empty last value.
-  if (/,\s*$/.test(text)) a.push("");
-  return a;
+  if(!text) return null;
+
+  const cells = text.split(/,(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)/gm);
+  if(!cells || !cells.length) return null;
+  
+  return cells.map(cell => {
+    if(cell.startsWith('"') && cell.endsWith('"')) cell = cell.slice(1, -1);
+    return cell;
+  })
 }
 
 module.exports = CSVtoArray;
